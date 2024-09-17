6c39bbdff93cd28a33756822e91198422acaa4e7
Roman <surkov.ron@gmail.com>
2024-09-17 11:22:38 +0300

    delete

--- hw3Task2.js
+++ hw3Task2.js
@@ -3,3 +3,11 @@
 let number = parseInt(2.9)
 number = String(number)
 console.log(Number(number) + Number(number + number) + Number(number + number + number))
+
+const a = 1
+
+for (let i = 1; i <= 5; i++) {
+    const a = 1
+    let b = a + 1
+    console.log(b);  
+}
