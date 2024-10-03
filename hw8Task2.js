dc704558a875810e7d599202a4f381b9d93f56c1
Roman <surkov.ron@gmail.com>
2024-10-03 16:17:25 +0300

    fix task2

--- hw8Task2.js
+++ hw8Task2.js
@@ -12,25 +12,35 @@ const words = [
     'tst'
 ]
 const vowels = ['a', 'e', 'i', 'o', 'u'];
-newArray = []
-newArray2 = []
+// newArray = []
+// newArray2 = []
+
+// function countVowels(word) {
+//     return word.split('').filter(char => vowels.includes(char.toLowerCase())).length;
+// }
 
 function countVowels(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
     return word.split('').filter(char => vowels.includes(char.toLowerCase())).length;
 }
 
-function sortArray(array) {
-    for (const word of array) {
-        if (vowels.some(vowel => word.includes(vowel))) {
-            newArray.push(word);
-        }
-    }
 
-    newArray2 = newArray.sort((a, b) => {
-        return countVowels(a) - countVowels(b)
-    })
-    return newArray2
-}
+function sortArray(array) {
+    return [...array].sort((a, b) => countVowels(a) - countVowels(b))
+ }
+
+// function sortArray(array) {
+//     for (const word of array) {
+//         if (vowels.some(vowel => word.includes(vowel))) {
+//             newArray.push(word);
+//         }
+//     }
+
+//     newArray2 = newArray.sort((a, b) => {
+//         return countVowels(a) - countVowels(b)
+//     })
+//     return newArray2
+// }
 
 
 console.log(sortArray(words))
